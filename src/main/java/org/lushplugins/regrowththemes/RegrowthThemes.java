c68ab9fc35b760f6743bfa3c08372d83e4dcded1
OakLoaf <oak@beaconstudios.org>
2024-11-21 16:16:49 +0000

    Updated project details

--- src/main/java/org/lushplugins/regrowththemes/RegrowthThemes.java
+++ src/main/java/org/lushplugins/regrowththemes/RegrowthThemes.java
@@ -1,9 +1,9 @@
-package org.lushplugins.pluginname;
+package org.lushplugins.regrowththemes;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PluginName extends JavaPlugin {
-    private static PluginName plugin;
+public final class RegrowthThemes extends JavaPlugin {
+    private static RegrowthThemes plugin;
 
     @Override
     public void onLoad() {
@@ -18,5 +18,9 @@ public final class PluginName extends JavaPlugin {
     @Override
     public void onDisable() {
         // Disable implementation
+    }
+
+    public static RegrowthThemes getInstance() {
+        return plugin;
     }
 }
